1c04cd7104d020650c6cfb51bac6f1d30bf61ad1
Lohith <lohithrapaka@gmail.com>
2026-01-12 21:36:36 +0530

    Basics

---
+++ reversno.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverse(int x) {
+        long a = 0;
+        while (x != 0) {
+            a = a * 10 + x % 10;
+            x = x / 10;
+            if (a > 2147483647 || a < -2147483648) {
+                return 0;
+            }
+        }
+        return (int) a;
+    }
+}
