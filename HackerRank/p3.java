fed2b9d3c640428cecb5602b154dc73777a788ce
Lohith <lohithrapaka@gmail.com>
2026-01-27 21:58:50 +0530

    HackerRank_1

---
+++ HackerRank/p3.java
@@ -0,0 +1,63 @@
+import java.io.*;
+ 
+import java.util.*;
+ 
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'compareTriplets' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER_ARRAY b
+     */
+
+    public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        int al = 0;
+        int c = 0;
+        for(int i = 0 ; i < Math.max(a.size() , b.size()) ; i++){
+            if(a.get(i) > b.get(i)){
+                al++;
+            }else if(a.get(i) < b.get(i)){
+                c++;
+            }
+        }
+        arr.add(al);
+        arr.add(c);
+        return arr;
+    }
+
+}
+
+public class p3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> b = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.compareTriplets(a, b);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
